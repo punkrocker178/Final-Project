a0ec094ae01db9b37ac470f7c2ee8f36c12e117d
Hieu <666.noobno@gmail.com>
2019-11-26 22:33:37 +0700

    TD-20
    Select levels

---
+++ Assets/Scripts/LevelSelector.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelector : MonoBehaviour
+{
+    public void Select (string levelName) {
+        SceneManager.LoadScene(levelName);
+    }
+}
