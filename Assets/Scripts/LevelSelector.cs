8507ec791f3d8856f8e01aeac3423cc0a765ffa1
Hieu <666.noobno@gmail.com>
2019-12-02 22:15:25 +0700

    TD-20 TD-21
    Able to create save file and load save file

--- Assets/Scripts/LevelSelector.cs
+++ Assets/Scripts/LevelSelector.cs
@@ -1,8 +1,23 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class LevelSelector : MonoBehaviour
 {
+    void Start() {
+        GameObject[] levels = GameObject.FindGameObjectsWithTag("LevelButtons");
+        PlayerStats.LoadSave();
+        PlayerStats.SaveObject save = PlayerStats.save;
+        for(int i = 0; i< levels.Length; i++ ) {
+            Debug.Log(save);
+            if (save.levels[i].unlocked) {
+                levels[i].GetComponent<Button>().interactable = true;
+            } else {
+                levels[i].GetComponent<Button>().interactable = false;
+            }
+        }
+    }
+
     public void Select (string levelName) {
         SceneManager.LoadScene(levelName);
     }
--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System.IO;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,15 +7,52 @@ public class PlayerStats : MonoBehaviour
 {
     public static int Money;
     public static int Lives;
+    public static SaveObject save;
+    public static int numberOfLevels = 12;
     public int starterMoney = 400;
     public int StartLives = 20;
 
+
     public static int Rounds;
+    private static string path = Application.dataPath + "/Save/Levels.json";
 
 
     void Start()
     {
         Money = starterMoney;
         Lives = StartLives;
+    }
+
+    public static void LoadSave() {
+        if(File.Exists(path)) {
+           save = JsonUtility.FromJson<SaveObject>(File.ReadAllText(path));
+           Debug.Log(save.levels[0].unlocked);
+        } else {
+           save = new SaveObject();
+           save.levels[0] = new Level("level01", true);
+        }
+    }
+
+    public static void WriteSave() {
+        JsonUtility.ToJson<SaveObject>(save);
+    }
+
+    public class SaveObject {
+       public Level[] levels;
+
+       public SaveObject() {
+           levels = new Level[PlayerStats.numberOfLevels];;
+       }
+    }
+
+    [System.Serializable]
+    public class Level {
+        public string levelName;
+        public bool unlocked;
+
+        public Level(string levelName, bool unlocked) {
+            this.levelName = levelName;
+            this.unlocked = unlocked;
+        }
     }
 }
